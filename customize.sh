9368fb0d8ef253897ee954a20441017e50f01d76
Tony Yao <11062997+jjyao88@users.noreply.github.com>
2023-10-20 01:09:16 +0800

    Switch to MMT-Extended framework (#2)
    * feat: switch to MMT Extended framework
    
    * fix: move cfg whether it is already patched or not

---
+++ customize.sh
@@ -0,0 +1,7 @@
+##########################################################################################
+# MMT Extended Logic - Don't modify anything after this
+##########################################################################################
+
+SKIPUNZIP=1
+unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
+. $TMPDIR/functions.sh
